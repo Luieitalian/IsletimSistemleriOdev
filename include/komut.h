e38b0a3579e1aef0b197a152b092f3231fa469f6
uzeyiremre <uzeyir.turkmen@ogr.sakarya.edu.tr>
2024-12-27 16:59:17 +0300

    Create komut.h

---
+++ include/komut.h
@@ -0,0 +1,14 @@
+/**
+ * BERKE PİTE
+ * ENES SOYLU
+ * AHMET PEKDEMİR
+ * ÜZEYİR EMRE TÜRKMEN
+ * HASAN YASİR ARSLAN
+ */
+
+#ifndef KOMUT_H
+#define KOMUT_H
+#include "kabuk.h"
+
+void komut_dizisi_calistir(char *input);
+#endif
