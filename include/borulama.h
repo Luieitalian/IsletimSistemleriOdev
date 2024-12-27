e74c11945938e4dce9bb47c03648d337bba115fb
Enes Soylu <43515847+ByThePi@users.noreply.github.com>
2024-12-27 16:40:59 +0300

    feat: borulama eklendi

---
+++ include/borulama.h
@@ -0,0 +1,17 @@
+borulama.h
+/**
+ * BERKE PİTE
+ * ENES SOYLU
+ * AHMET PEKDEMİR
+ * ÜZEYİR EMRE TÜRKMEN
+ * HASAN YASİR ARSLAN
+ */
+
+#ifndef BORULAMA_H
+#define BORULAMA_H
+#include "kabuk.h"
+
+void pipeline_calistir(char **args, int arg_count);
+void cocukProses_bitis_handle(int sig);
+
+#endif
